92097b27eb663c35a6952d22b24401edbff24a59
Woody <devkritt@gmail.com>
2024-05-11 14:36:54 +0700

    Complete Initialize Start up ExpressJS project for development

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,13 @@ const plannerRoute = require('./src/planner/route')
 const transactionRoute = require('./src/transaction/route')
 
 const app = express()
+require('dotenv').config()
 
 app.use('/api/v1/user', userRoute)
 app.use('/api/v1/product', productRoute)
 app.use('/api/v1/planner', plannerRoute)
 app.use('/api/v1/transaction', transactionRoute)
 
-app.listen(3000, ()=>{
-    console.log(`Server is running on http://localhost:${3000}`)
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on http://localhost:${process.env.PORT}`)
 })
