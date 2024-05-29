1ca3fd49246a582be4de20e1ef459d5034c612b5
Woody <devkritt@gmail.com>
2024-05-29 14:17:59 +0700

    edit origin

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const devOrigin = 'http://localhost:5173'
 const prodOrigin = [process.env.PROD_ORIGIN_1, process.env.PROD_ORIGIN_2];
 
 const corsOptions = {
-    origin: devOrigin,
+    origin: prodOrigin,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }
