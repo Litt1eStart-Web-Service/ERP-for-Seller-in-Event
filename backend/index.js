372c1b5c905a32ef94a918ab07ef6da5ad25dab8
Woody <devkritt@gmail.com>
2024-05-29 12:07:45 +0700

    add lineNotify for send summarize of data to line

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const devOrigin = 'http://localhost:5173'
 const prodOrigin = [process.env.PROD_ORIGIN_1, process.env.PROD_ORIGIN_2];
 
 const corsOptions = {
-    origin: prodOrigin,
+    origin: devOrigin,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }
