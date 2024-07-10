2ca44bf835d92c27ca8439c7dbf8f906a66a35c7
Woody <devkritt@gmail.com>
2024-07-10 09:29:14 +0700

    feat: create function for calculate total margin in workspace

--- client/src/pages/PlannerPage.jsx
+++ client/src/pages/PlannerPage.jsx
@@ -28,6 +28,7 @@ const PlannerPage = () => {
   const [plannerData, setPlannerData] = useState(null);
   const [product, setProduct] = useState("");
   const [amount, setAmount] = useState("");
+  const [total_margin, setTotalMargin] = useState(0);
   const [total_sales, setTotalSales] = useState("");
   const [other_expenses, setOtherExpenses] = useState("");
 
@@ -198,13 +199,27 @@ const PlannerPage = () => {
     }
   };
 
+  const calculateTotalMargin = async() => {
+    if(order.length <= 0) return;
+
+    var sum = 0;
+    for (let i = 0; i < order.length; i++) {
+      sum += order[i].product_id.margin * order[i].amount;     
+    }
+
+    setTotalMargin(sum);
+    console.log(sum);
+  }
+  
   useEffect(() => {
     fetchPlannerData();
     fetchOrderData();
     fetchProductData();
   }, []);
 
-  console.log(product);
+  useEffect(() => {
+    calculateTotalMargin();
+  }, [order])
 
   return (
     <>
@@ -286,6 +301,9 @@ const PlannerPage = () => {
           >
             Home Page
           </Button>
+          <Typography>
+            ต้นทุนของสินค้าทั้งหมด: {total_margin}
+          </Typography>
         </Stack>
       </Stack>
     </>
